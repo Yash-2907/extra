5c1f3ec5ce7959ceb3188080f6a31c73d8a235b3
Yash-2907 <guptayp_1@rknec.edu>
2025-04-28 22:01:39 +0530

    Commit number 0

--- cpp.cpp
+++ cpp.cpp
@@ -1,21 +1,23 @@
-#include<iostream>
-#include<cstdlib>
-#include<fstream>
-#include<unistd.h>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <unistd.h>
 using namespace std;
-int main(){
+
+int main()
+{
     int tot;
-    cout<<"enter number of commits to put in : ";
-    cin>>tot;
-    for(int i=0;i<tot;i++)
+    cout << "Enter number of commits to put in: ";
+    cin >> tot;
+    for (int i = 0; i < tot; i++)
     {
-        std::ofstream f("temp.txt");
-        f << rand();
+        ofstream f("temp.txt");
+        f.close();
         system("git add .");
-        system("git commit -m \"this is push from cpp\"");
+        system(("git commit -m \"Commit number " + to_string(i) + "\"").c_str());
         system("git push origin main");
         usleep(10000000);
-        cout << "done push " << i << "...";
+        cout << "Done push " << i + 1 << "..." << endl;
     }
     return 0;
 }
