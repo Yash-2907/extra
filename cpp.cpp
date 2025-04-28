3f70a19388e9ec337cf9db8865d5c4e20de30d0c
Yash-2907 <guptayp_1@rknec.edu>
2025-04-28 21:41:59 +0530

    this is push from cpp

--- cpp.cpp
+++ cpp.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
 #include<cstdlib>
+#include<fstream>
 using namespace std;
 int main(){
-    system("git add .");
-    system("git commit -m \"this is push from cpp\"");
-    system("git push origin main");
-    cout<<"done...";
+    int tot;
+    cout<<"enter number of commits to put in : ";
+    cin>>tot;
+    for(int i=0;i<tot;i++)
+    {
+        std::ofstream f("temp.txt");
+        f << rand();
+        system("git add .");
+        system("git commit -m \"this is push from cpp\"");
+        system("git push origin main");
+        cout << "done push " << "i " << "...";
+    }
     return 0;
 }
