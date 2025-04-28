dec1536267cc6d557aae06bfe23d6f6c1fb264c6
Yash-2907 <guptayp_1@rknec.edu>
2025-04-28 22:05:26 +0530

    Commit number 0

--- cpp.cpp
+++ cpp.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
-#include <unistd.h>
+#include <unistd.h> // For sleep function
 using namespace std;
 
 int main()
@@ -9,15 +9,23 @@ int main()
     int tot;
     cout << "Enter number of commits to put in: ";
     cin >> tot;
+
     for (int i = 0; i < tot; i++)
     {
+        // Open temp.txt and close it immediately (this is your file write step)
         ofstream f("temp.txt");
         f.close();
+
+        // Git commands
         system("git add .");
         system(("git commit -m \"Commit number " + to_string(i) + "\"").c_str());
         system("git push origin main");
-        usleep(10000000);
+
+        // Sleep for 10 seconds
+        sleep(10); // Sleep for 10 seconds (Unix-based systems)
+
         cout << "Done push " << i + 1 << "..." << endl;
     }
+
     return 0;
 }
