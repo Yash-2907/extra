ad204fd93dc2be98c5e832dbdc224740a20f8624
Yash-2907 <guptayp_1@rknec.edu>
2025-04-29 23:53:40 +0530

    Commit number 0

--- cpp.cpp
+++ cpp.cpp
@@ -11,10 +11,11 @@ int main()
         ofstream f("temp.txt", ios::app);
         f << "Adding a new line and number , Random number: " << rand() << " (Commit " << i << ")" << endl;
         f.close();
+        sleep(2);
         system("git add .");
         system(("git commit -m \"Commit number " + to_string(i) + "\"").c_str());
         system("git push origin main");
-        sleep(5);
+        sleep(2);
         cout << "Done push " << i + 1 << "..." << endl;
     }
     return 0;
